1d51a7e8c3b1e3d555d210f4cc3f04ee083b163c
marcosvpa <marcos_andrade9@hotmail.com>
2020-06-30 10:15:30 -0300

    Final Project

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -14,31 +14,42 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 
-		Map <String,Integer> mapTest = new LinkedHashMap<String,Integer>();
 		Scanner sc = new Scanner(System.in);
-		
+
 		String getInput;
 
 		System.out.print("Enter the file path:");
 		getInput = sc.nextLine();
-		Map <String,Integer> voteMap = new HashMap<>();
-		try (BufferedReader br = new BufferedReader (new FileReader(getInput))) {
+		Map<String, Integer> voteMap = new HashMap<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(getInput))) {
+			
 			String line = br.readLine();
+			
 			while (line != null) {
-				System.out.println(line);
+				
 				String getInfo[] = line.split(",");
-				
-				
-				
+
+				if (!voteMap.containsKey(getInfo[0])) {
+					voteMap.put(getInfo[0], Integer.parseInt(getInfo[1]));
+				} else {
+					voteMap.put(getInfo[0], voteMap.get(getInfo[0]) + Integer.parseInt(getInfo[1]));
+				}
+
 				line = br.readLine();
 			}
 		}
-		
+
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
+
 		
-		
+	       for (Map.Entry<String, Integer> totalVote : voteMap.entrySet()) {
+	            String key = totalVote.getKey();
+	            Integer value = totalVote.getValue();
+	            System.out.println(String.format("Candidato: %s | Total Votos: %d", key, value));
+	        }
+	       
 		
 		sc.close();
 	}
